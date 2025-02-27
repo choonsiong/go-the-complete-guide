823464692cf22e1f8c1e238a60a04b604b23c6aa
choonsiong <choonsiong.lee@gmail.com>
2025-02-27 22:56:16 +0800

    Auto commit

--- section-11/rest-api/db/db.go
+++ section-11/rest-api/db/db.go
@@ -25,7 +25,7 @@ func createTables() {
 	createUsersTable := `CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		email TEXT NOT NULL UNIQUE,
-		password TEXT NOT NULL,    	
+		password TEXT NOT NULL
 	)`
 
 	_, err := DB.Exec(createUsersTable)
