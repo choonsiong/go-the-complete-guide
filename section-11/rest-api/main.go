d92f8958ca9dfe39e9039a3e21c75cb85d72ad99
choonsiong <choonsiong.lee@gmail.com>
2025-02-27 23:42:56 +0800

    Auto commit

--- section-11/rest-api/main.go
+++ section-11/rest-api/main.go
@@ -4,14 +4,16 @@ import (
 	"example.com/rest-api/db"
 	"example.com/rest-api/routes"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
 // Dependencies:
 // go get -u github.com/gin-gonic/gin
+// go get github.com/mattn/go-sqlite3
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
 	routes.RegisterRoutes(server)
-	server.Run(":8080")
+	log.Fatal(server.Run(":8080"))
 }
