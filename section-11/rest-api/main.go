e62a2e883628c9e7cb10aec644800b06c9a9ed9f
choonsiong <choonsiong.lee@gmail.com>
2025-02-27 18:28:10 +0800

    Auto commit

--- section-11/rest-api/main.go
+++ section-11/rest-api/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import "github.com/gin-gonic/gin"
 
+// Dependencies:
+// go get -u github.com/gin-gonic/gin
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
 }
