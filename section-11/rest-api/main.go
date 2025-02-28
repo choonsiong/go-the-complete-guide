c4d647c854d1e2a7198ec84c19955a00f6ec5bc2
choonsiong <choonsiong.lee@gmail.com>
2025-02-28 16:13:04 +0800

    Auto commit

--- section-11/rest-api/main.go
+++ section-11/rest-api/main.go
@@ -11,6 +11,7 @@ import (
 // go get -u github.com/gin-gonic/gin
 // go get github.com/mattn/go-sqlite3
 // go get -u golang.org/x/crypto
+// go get -u github.com/golang-jwt/jwt/v5
 
 func main() {
 	db.InitDB()
