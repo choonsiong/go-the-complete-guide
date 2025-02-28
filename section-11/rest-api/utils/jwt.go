8d18c547bfbd852f1bdf9ad4e3b5aafed50375c1
choonsiong <choonsiong.lee@gmail.com>
2025-02-28 17:10:51 +0800

    Auto commit

--- section-11/rest-api/utils/jwt.go
+++ section-11/rest-api/utils/jwt.go
@@ -2,14 +2,12 @@ package utils
 
 import (
 	"github.com/golang-jwt/jwt/v5"
-	"log"
 	"time"
 )
 
 const secretKey = "my-secret-key"
 
 func GenerateToken(email string, userID int) (string, error) {
-	log.Println(email, userID)
 	return jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email":  email,
 		"userID": userID,
